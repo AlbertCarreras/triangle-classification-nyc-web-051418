234e5c465d372f8a542e270fa13a1ea3ebca9f6c
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-28 15:49:08 -0400

    Solved lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,6 @@ class Triangle
 
         raise TriangleError
 
-    elsif @length1 == 0 && @length2 == 0 && @length3 == 0
-
-      raise TriangleError
-
     elsif @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
 
       raise TriangleError
